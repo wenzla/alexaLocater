4e6485db61e62fa7aadfe69a63b1b88eb562b232
Allen Wenzl <allenliwenzl@gmail.com>
2018-04-25 23:50:12 -0400

    dynamo db acutally fucking works now

---
+++ config.js
@@ -0,0 +1,5 @@
+let config = {}
+
+config.tableName = "DeviceLocation"
+
+module.exports = config
--- index.js
+++ index.js
@@ -1,30 +1,53 @@
 var Alexa = require('alexa-sdk');
+var AWS = require("aws-sdk");
+var docClient = new AWS.DynamoDB.DocumentClient({ region: "us-east-1"});
+var dynamodbstreams = new AWS.DynamoDBStreams({apiVersion: "2012-08-10"});
 
-exports.handler = function(event, context, callback){
-  var alexa = Alexa.handler(event, context);
-  alexa.registerHandlers(handlers);
-  alexa.execute();
+var table = "DeviceLocation";
+var title;
+var userID = "test";
+var alexa;
+var APP_ID = "amzn1.ask.skill.be17ef63-f20c-4f6f-8169-f130d5ebdf85";
+
+
+exports.handler = (event, context, callback) => {
+    alexa = Alexa.handler(event, context, callback);
+    alexa.appId = APP_ID;
+    console.log('Getting Data');
+    getData();
+    console.log('Got Data');
+    //alexa.registerHandlers(handlers);
+    //alexa.execute();     
 };
 
-var handlers = {
-
+const handlers = {
     'LaunchRequest': function () {
-        this.emit(':ask', 'Hello World! This is a simple custom skill.');
+        this.emit('RememberName');
     },
-  
-    'RememberName': function () {
-    var nameSlot = this.event.request.intent.slots.name.value;
-    var name;
-    if (nameSlot) {
-      name = nameSlot;
-    }
-
-    if (name) {
-      this.attributes['userName'] = name;
-      this.emit(':tell', `Nice to meet you ${name}! I'll greet you by name the next time we talk`);
-    } else {
-      this.emit(':ask', `Sorry, I don\'t recognise that name!`, `'Tell me your name by saying: My name is, and then your name.'`);
-    }
-  }
-
+    'RememberName': function () {        
+        console.log("About to speak: " + title);
+        this.response.speak('You are in the ' + title)
+        this.emit(':responseReady'); 
+    },
 };
+
+function getData() {
+    var ddb = new AWS.DynamoDB.DocumentClient({region:'us-east-1'});
+    var params = {
+        TableName: table, 
+        Key:{
+            'UserId': userID,
+            }
+    };      
+    ddb.get(params, function(err, data) {
+        if (err) {
+            console.log('error: ' + JSON.stringify(err, null, 2))
+        }else{
+           title = data.Item.room;
+           console.log(data);
+           console.log(title);
+           alexa.registerHandlers(handlers);
+           alexa.execute();            
+        }
+    });
+}
